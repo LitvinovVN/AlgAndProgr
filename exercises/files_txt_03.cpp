ec177dfe4cd652d58122fe5b75b13c52994dfd57
LitvinovVN <LitvinovVN@rambler.ru>
2023-12-18 19:51:59 +0300

    1812231951

--- exercises/files_txt_03.cpp
+++ exercises/files_txt_03.cpp
@@ -16,38 +16,112 @@
 
 using namespace std;
 
-int main()
+int inputIntGt0(string message, string errorMessage)
+{
+	int n;
+	while(1)
+	{
+		cout << message;
+		cin >> n;
+		if (n > 0) return n;
+		cout << errorMessage << endl;
+	}
+	
+}
+
+void read_config(string& fileNameData, int& n)
 {
 	ifstream f; //Поток для чтения.
-	float a;
-	int n=0;
-	string fileNameData;
-
 	f.open("config.txt"); //Открываем файл config.txt в режиме чтения.
 	if(f) //Если открытие файла config.txt прошло корректно, то
 	{
 		f >> fileNameData;
-		f >> n;
-		cout << "Reading config.txt" << endl;
-		cout << "\tfileNameData: " << fileNameData << endl; //Вывод на экран количества чисел.
-		cout << "\tn: " << n << endl; //Вывод на экран количества чисел.
+		f >> n;	
 		
 		f.close(); //Закрытие потока.		
 	}
 	else
     {
-        cout << "Файл config.txt не найден" << endl; //Если открытие файла прошло некорректно, то
-	                                    //вывод сообщения, об отсутствии такого файла
-        // 1. Запрашиваем у пользователя имя файла с данными        
-        cout << "Input file: ";
-        cin >> fileNameData;		
-        cout << "n = "; cin >> n; //Ввод количества вещественных чисел
+        cout << "File config.txt not found!" << endl; //Если открытие файла прошло некорректно, то
+	                //вывод сообщения, об отсутствии такого файла
+        // Запрашиваем у пользователя имя файла с данными        
+        cout << "Input data file name: "; cin >> fileNameData;		
+        // Вызов функции ввода целого положительного числа
+		n = inputIntGt0("Enter n (n>0): ", "ERROR! n>0!");
 
 		ofstream fout; //Описывает поток для записи данных в файл
 		fout.open("config.txt"); //Открываем файл в режиме записи
 		fout << fileNameData << " " << n;// Записываем в файл имя файла с данными и количество чисел
 		f.close(); //Закрытие потока
     }
+}
 
+double* arrayCreate(int n)
+{
+	return new double[n];
+}
+
+void read_data(string fileNameData, double* arr, int n)
+{
+	ifstream f; //Поток для чтения.
+	f.open(fileNameData); //Открываем файл fileNameData в режиме чтения.
+	if(f) //Если открытие файла прошло корректно, то
+	{
+		for(int i = 0; i < n; i++)
+		{
+			f >> arr[i];
+		}
+		
+		f.close(); //Закрытие потока.		
+	}
+	else
+    {
+        cout << "File " << fileNameData << " not found!" << endl; //Если открытие файла прошло некорректно, то
+	                //вывод сообщения, об отсутствии такого файла
+        
+		// Запрашиваем у пользователя данные
+		for(int i = 0; i < n; i++)
+		{
+			cout << "arr[" << i << "] = ";
+			cin >> arr[i];
+		}        
+                
+		ofstream fout; //Описывает поток для записи данных в файл
+		fout.open(fileNameData); //Открываем файл в режиме записи
+		for(int i = 0; i < n; i++)
+		{
+			fout << arr[i];// Записываем в файл i-й элемент массива arr
+			if(i < n-1)
+				fout << " ";
+		}		
+		f.close(); //Закрытие потока
+    }
+}
+
+void arrayPrint(string message, double* arr, int n)
+{
+	cout << message;
+	for(int i = 0; i < n; i++)
+	{
+		cout << arr[i];// Выводим в консоль i-й элемент массива arr
+		if(i < n-1)
+			cout << " ";
+	}	
+}
+
+int main()
+{
+	float a;
+	int n;
+	string fileNameData;
+	read_config(fileNameData, n);
+	cout << "fileNameData = " << fileNameData << endl;
+	cout << "n = " << n << endl;
+
+	double* arr = arrayCreate(n);
+	read_data(fileNameData, arr, n);
+	arrayPrint("arr: ", arr, n);
+
+	delete[] arr;
     return 0;
 }
